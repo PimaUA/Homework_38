b501dcadba977b6feba010da0950f1077de3eb02
PimaUA <pimaua87@gmail.com>
2023-07-03 11:21:56 +0300

    commit for Feature Branch. added classed for entities.

---
+++ src/main/java/org/springframework/web/Homework_38/entities/Order.java
@@ -0,0 +1,29 @@
+package org.springframework.web.Homework_38.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Setter
+@ToString
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    @Getter
+    private int id;
+    @Column
+    @Getter
+    private LocalDate date;
+    @Column
+    @Getter
+    private double cost;
+    @ManyToMany(mappedBy = "listOfOrders")
+    private List<Product> listOfProducts;
+}
